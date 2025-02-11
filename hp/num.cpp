7dbc2a79f9a39b9f9ce4eb4f6e55196101570015
danielpro51 <parrabaldovinodanielisaac@gmail.com>
2025-02-11 18:00:49 -0500

    g

--- hp/num.cpp
+++ hp/num.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 
 //void significa que la función no devuelve ningún valor
-void DosVecesX(int x){
+//Opción 1
+/*void DosVecesX(int x){
     std::cout << x << " dos veces es " << x*2 << "\n";
-}
+}*/
+//Opción 2
+void DosVecesX(int x);
 int pedirValor(int x){
     std::cout << "Dime un número entero: ";
     std::cin >> x;
@@ -13,4 +16,8 @@ int main(){
     int x{};
     DosVecesX(pedirValor(x));
     return 0;
+}
+//Opción 2
+void DosVecesX(int x){
+    std::cout << x << " dos veces es " << x*2 << "\n";
 }
