a663fa701dda08caad61a6e29f3d836c5ae3f027
danielpro51 <parrabaldovinodanielisaac@gmail.com>
2025-02-12 16:42:35 -0500

    g

---
+++ hp/nspac.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std; //No debería usarse, pero se usa para simplificar
+
+int main(){
+    int x{};
+    cout << "Dime un número entero: ";
+    cin >> x;
+    cout << x << "\n";
+    return 0;
+
+}
---
+++ hp/procesa.cpp
@@ -0,0 +1,10 @@
+#include <istream> //Se usa para incluir la librería y archivos de cabecera
+#define PI 3.14159 //Se define una constante, en este caso el valor de pi en mayúsculas
+//No se recomienda usar #define para definir constantes, se recomienda usar const
+#ifdef PI //Se usa para definir una condición ifdef signfica if defined
+#endif //Se usa para finalizar la condición
+#ifndef EULER //Se usa para definir una condición ifndef significa if not defined
+#endif //Se usa para finalizar la condición
+
+#if 0 //Se usa para excluir un bloque de código es cómo poner un comentario
+#endif //Se usa para finalizar la exclusión
