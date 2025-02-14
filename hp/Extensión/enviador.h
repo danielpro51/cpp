8e98aa1bd4dc7b57df7fe1207a708858c04ec028
danielpro51 <parrabaldovinodanielisaac@gmail.com>
2025-02-14 01:57:44 -0500

    w

--- hp/Extensión/enviador.h
+++ hp/Extensión/enviador.h
@@ -1,5 +1,6 @@
-#ifndef ENVIADOR_H
-#define ENVIADOR_H
+#ifndef ENVIADOR_H //Esto sirve para que no se dupliquen las funciones
+//Si no está definido, el compilador lo procesará
+#define ENVIADOR_H //Define la variable para que no se dupliquen las funciones
 
 int suma(int a, int b);
 #endif
